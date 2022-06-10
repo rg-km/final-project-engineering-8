1af7d594c933ee91ba84959888692f39c1e50b0d
FermiNA29 <ferminaufal@gmail.com>
2022-06-10 07:56:33 +0700

    [Backend} mengubah struktur tabel database

--- backend/database/migration/main.go
+++ backend/database/migration/main.go
@@ -22,28 +22,14 @@ func main() {
 
 func CreateTableTest(db *sql.DB) (string, error) {
 	_, err := db.Exec(`
-	CREATE TABLE IF NOT EXISTS siswa (
-		SiswaID integer not null primary key AUTOINCREMENT,
+	CREATE TABLE IF NOT EXISTS user (
+		UserID integer not null primary key AUTOINCREMENT,
 		username varchar(255) not null,
 		password varchar(255) not null,
 		nama varchar(255) not null,
 		alamat varchar(255) not null,
-		RoleID varchar(255) not null,
-		FOREIGN KEY (RoleID) REFERENCES role(RoleID)
-	);
-
-	CREATE TABLE IF NOT EXISTS role (
-		RoleID integer not null primary key AUTOINCREMENT,
+		noHp varchar(255) not null,
 		role varchar(255) not null
-	);
-
-	CREATE TABLE IF NOT EXISTS admin (
-		AdminID integer not null primary key AUTOINCREMENT,
-		username varchar(255) not null,
-		password varchar(255) not null,
-		nama varchar(255) not null,
-		RoleID varchar(255) not null,
-		FOREIGN KEY (RoleID) REFERENCES role(RoleID)
 	);
 
 	CREATE TABLE IF NOT EXISTS jenjang (
@@ -61,53 +47,37 @@ func CreateTableTest(db *sql.DB) (string, error) {
 		kategori varchar(255) not null
 	);
 
-	CREATE TABLE IF NOT EXISTS guru (
-		GuruID integer not null primary key AUTOINCREMENT,
-		username varchar(255) not null,
-		password varchar(255) not null,
-		nama varchar(255) not null,
-		alamat varchar(255) not null,
-		noHp varchar(255) not null,
+	CREATE TABLE IF NOT EXISTS info_guru (
+		InfoGuruID integer not null primary key AUTOINCREMENT,
 		deskripsi varchar(255) not null,
 		biaya varchar(255) not null,
-		RoleID varchar(255) not null,
-		JenjangID varchar(255) not null,
-		PelajaranID varchar(255) not null,
-		KategoriID varchar(255) not null,
-		FOREIGN KEY (RoleID) REFERENCES role(RoleID),
+		ratting varchar(255) not null,
+		UserID integer not null,
+		JenjangID integer not null,
+		PelajaranID integer not null,
+		KategoriID integer not null,
+		FOREIGN KEY (UserID) REFERENCES user(UserID),
 		FOREIGN KEY (JenjangID) REFERENCES jenjang(JenjangID),
 		FOREIGN KEY (PelajaranID) REFERENCES pelajaran(PelajaranID),
 		FOREIGN KEY (KategoriID) REFERENCES kategori(KategoriID)
 	);
 
-	CREATE TABLE IF NOT EXISTS siswa_guru (
-		SiswaID integer not null,
-		GuruID integer not null,
-		FOREIGN KEY (SiswaID) REFERENCES siswa(SiswaID),
-		FOREIGN KEY (GuruID) REFERENCES guru(GuruID)
-	);
-
-	INSERT INTO role (role) VALUES ("admin"), ("siswa"), ("guru");
-	INSERT INTO admin (username, password, nama, RoleID) VALUES ("admin", "admin", "admin", 1);
 	INSERT INTO jenjang (jenjang) VALUES ("SD"), ("SMP"), ("SMA");
 	INSERT INTO pelajaran (pelajaran) VALUES ("Matematika"),("Bahasa Inggris");
 	INSERT INTO kategori (kategori) VALUES ("Anak Berkebutuhan Khusus"), ("Anak Non Berkebutuhan Khusus");
 
-	INSERT INTO siswa (username, password, nama, alamat, RoleID) 
+	INSERT INTO user (username, password, nama, alamat, noHP,role) 
 	VALUES 
-	("ucup", "ucup123", "Muhammad Ucup", "Jl Bojong Soang No.1, Kota Bandung", 2),
-	("Rendi", "Rendi123", "Muhammad Rendi", "Jl Rajawali No.1, Jakarta Timur", 2);
+	("admin", "admin", "admin", "admin", "admin", "admin"),
+	("ucup", "ucup123", "Muhammad Ucup", "Jl Bojong Soang No.1, Kota Bandung", "0812341234", "siswa"),
+	("Rendi", "Rendi123", "Muhammad Rendi", "Jl Rajawali No.1, Jakarta Timur", "0812341234", "siswa"),
+	("Subabjo", "Subabjo123", "Muhammad Subabjo", "Jl Dago, Kota Bandung", "0812341234", "guru"),
+	("Kurniawan", "Kurniawan123", "Muhammad Kurniawan", "Jl Jendral Sudirman, Jakarta Timur", "0812341234", "guru");
 
-	INSERT INTO guru (username, password, nama, alamat, noHp, deskripsi, biaya, RoleID, JenjangID, PelajaranID, KategoriID)
+	INSERT INTO info_guru (deskripsi, biaya, ratting, UserID, JenjangID, PelajaranID, KategoriID)
 	VALUES
-	("Subabjo", "Subabjo123", "Muhammad Subabjo", "Jl Dago, Kota Bandung", "081212121212", "Saya guru yang mengajar Matematika", "10000", 2, 1, 1, 1),
-	("Kurniawan", "Kurniawan123", "Muhammad Kurniawan", "Jl Jendral Sudirman, Jakarta Timur", "081212121212", "Saya guru yang mengajar Bahasa Inggris", "20000", 2, 1, 2, 2);
-	
-	INSERT INTO siswa_guru (SiswaID, GuruID)
-	VALUES
-	(1, 1),
-	(2, 2);
-
+	("Saya memiliki keahlian dalam bidang matematika", "Rp. 100.000", "4.5", 3, 1, 1, 1),
+	("Saya memiliki keahlian dalam bidang bahasa inggris", "Rp. 100.000", "4.5", 4, 1, 2, 2);
 	`)
 	if err != nil {
 		return "Tabel gagal dibuat", err
