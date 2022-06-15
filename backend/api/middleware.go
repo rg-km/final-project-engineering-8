91c55f912782efa61caa6c8a25d517d5eb33988b
gilangsp <gilang.putra@salt.co.id>
2022-06-15 10:30:53 +0700

    fix: allow CORS

--- backend/api/middleware.go
+++ backend/api/middleware.go
@@ -10,7 +10,8 @@ import (
 
 func (api *API) AllowOrigin(c *gin.Context) {
 	c.Writer.Header().Set("Content-Type", "application/json")
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080/")
+	// c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080/")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*") //for development purpose
 	c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
