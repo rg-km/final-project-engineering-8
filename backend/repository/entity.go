6967d02bce46502deaab22586314bdaa9cc36472
gilangsp <gilang.salt.co.id>
2022-06-23 03:13:05 +0700

    chore: add scneario image flow

--- backend/repository/entity.go
+++ backend/repository/entity.go
@@ -1,13 +1,14 @@
 package repository
 
 type User struct {
-	UserID   int    `json:"user_id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Nama     string `json:"nama"`
-	Alamat   string `json:"alamat"`
-	NoHp     string `json:"no_hp"`
-	Role     string `json:"role"`
+	UserID      int    `json:"user_id"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Nama        string `json:"nama"`
+	Alamat      string `json:"alamat"`
+	NoHp        string `json:"no_hp"`
+	ProfilePict string `json:"profile_pict"`
+	Role        string `json:"role"`
 }
 
 type InfoGuru struct {
@@ -26,6 +27,7 @@ type Teacher struct {
 	Name             string `json:"name"`
 	Address          string `json:"address"`
 	NoHp             string `json:"no_hp"`
+	ProfilePict      string `json:"profile_pict"`
 	Description      string `json:"description"`
 	Fee              string `json:"fee"`
 	Rating           string `json:"rating"`
